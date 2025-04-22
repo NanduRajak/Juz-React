ff1b935c96f426ac3274f71307ef285d6b45cc19
Nandu Rajak <golanukondanandu@gmail.com>
2025-04-22 14:39:15 +0530

    project completed

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import useCurrencyInfo from "./hooks/useCurrencyInfo.js";
+import InputBox from "./components/inputbox.jsx";
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [connvertedAmount, setConnvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConnvertedAmount(amount);
+    setAmount(connvertedAmount);
+  };
+  const convert = () => {
+    setConnvertedAmount(amount * currencyInfo[to]);
+  };
+
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeate"
+      style={{
+        backgroundImage: `url(https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/03/one-piece-luffy-laughing.jpg?q=50&fit=crop&w=800&dpr=1.5)`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="from"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setFrom(currency)}
+                onAmountChange={(amount) => setAmount(amount)}
+                selectedCurrency={from}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white roumded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
+              >
+                Swap
+              </button>
+            </div>
+            <div className="w-full mb-1">
+              <InputBox
+                label="to"
+                amount={Math.floor(connvertedAmount)}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectedCurrency={to}
+                amountDisabled
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert {from.toLocaleUpperCase()} to {to.toLocaleUpperCase()}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
